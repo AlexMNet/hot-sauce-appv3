166361c3cb9fb5f90614b1da5d1a6e032862ab4e
Alex Maldonado <emtamaldonado@gmail.com>
2021-03-17 00:14:19 -0700

    Add Unandled Rejection and Uncaught Exception Erorr Haneling

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  // eslint-disable-next-line no-console
+  console.log('UNCAUGHT EXCEPTION 🚧 Shutting down...');
+  // eslint-disable-next-line no-console
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 //This is where the env variables are read and
 //now accesable to the entire project
 //Make sure these variables are read before you set app
 //App is dependant on these variables!
 dotenv.config({ path: './config.env' });
-
-const mongoose = require('mongoose');
 const app = require('./app');
 
 //LOCAL DATABASE
@@ -18,17 +25,27 @@ mongoose
   .then(() => {
     // eslint-disable-next-line no-console
     console.log('Local DB Connection Successful');
-  })
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.log('MONGO CONNECTION ERROR!!!!');
-    // eslint-disable-next-line no-console
-    console.log(err);
   });
+// .catch((err) => {
+//   // eslint-disable-next-line no-console
+//   console.log('MONGO CONNECTION ERROR!!!!');
+//   // eslint-disable-next-line no-console
+//   console.log(err);
+// });
 
 //SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App is listening on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  // eslint-disable-next-line no-console
+  console.log(err.name, err.message);
+  // eslint-disable-next-line no-console
+  console.log('UNHANDLED REJECTION 🚧 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
